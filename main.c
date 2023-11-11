dbc3c49f5cab686d968e4646d989096fc3fea614
Bing Wen Tan <btan@c2r5s4.42singapore.sg>
2023-11-11 22:41:17 +0800

    modified:   Makefile 	modified:   main.c

--- main.c
+++ main.c
@@ -6,18 +6,36 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 16:58:43 by btan              #+#    #+#             */
-/*   Updated: 2023/11/11 18:42:51 by btan             ###   ########.fr       */
+/*   Updated: 2023/11/11 22:09:34 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <mlx.h>
 
-int	main()
+typedef struct window_size {
+	void	*ptr;
+	int	width;
+	int	height;
+}	t_window;
+
+int	close(int key, void *mlx_ptr, void *window)
+{
+	printf("%d\n", key);
+	if (key == 65307)
+		mlx_destroy_window(mlx_ptr, window);
+	return (1);
+}
+
+int	main(void)
 {
 	void	*mlx_ptr;
-	void	*window;
+	t_window	window;
 
 	mlx_ptr = mlx_init();
-	window = mlx_new_window(mlx_ptr, 1024, 1024, "Test");
+	window.width = 1024;
+	window.height = 512;
+	window.ptr = mlx_new_window(mlx_ptr, window.width, window.height, "FDF");
+	mlx_hook(window.ptr, 2, 1L << 0, close, mlx_ptr);
 	mlx_loop(mlx_ptr);
 }
