e68ad7bc68db79888650fd7a83ff3e388be44e2a
Bing Wen Tan <bing@Bings-MacBook-Pro.local>
2023-11-13 18:54:22 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -27,12 +27,17 @@ int	handle_keydown(int key, void *mlx_ptr, void *window)
 	return (0);
 }
 
-int	handle_mouse(int key, void *mlx_ptr, void *window)
+int	handle_mouse(int x, int y, void *mlx_ptr, void *window)
 {
-	printf("%d\n", key);
+	printf("x: %d\ny: %d\n", x, y);
 	//if (key == 65307)
 	//	return (mlx_destroy_window(mlx_ptr, window));
 	return (0);
+}
+
+int	handle_close(int key, void *mlx_ptr, void *window)
+{
+	return (mlx_destroy_window(mlx_ptr, window));
 }
 
 int	main(void)
@@ -45,6 +50,8 @@ int	main(void)
 	window.height = 512;
 	window.ptr = mlx_new_window(mlx_ptr, window.width, window.height, "FDF");
 	mlx_hook(window.ptr, 2, 1L << 0, handle_keydown, mlx_ptr);
+	//mlx_hook(window.ptr, 4, 1L << 2, handle_button, mlx_ptr);
 	mlx_hook(window.ptr, 6, 1L << 6, handle_mouse, mlx_ptr);
+	mlx_hook(window.ptr, 17, 0L, handle_close, mlx_ptr);
 	mlx_loop(mlx_ptr);
 }
