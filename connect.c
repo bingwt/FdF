c040198678beb3cd27f6f5777c7ef1ee49dd8e5c
Bing Wen Tan <btan@student.42singapore.sg>
2024-01-16 06:56:03 +0800

    Added connect.c and vectors.c

---
+++ connect.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   connect.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 06:31:18 by btan              #+#    #+#             */
+/*   Updated: 2024/01/16 06:32:11 by btan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fdf.h>
+
+static void	connect_col(t_props *props, int row, int col)
+{
+	t_line	line;
+
+	line.x0 = props->points[(row * props->map.cols) + col]->x;
+	line.y0 = props->points[(row * props->map.cols) + col]->y;
+	line.x1 = props->points[(row * props->map.cols) + col + 1]->x;
+	line.y1 = props->points[(row * props->map.cols) + col + 1]->y;
+	props->pixel.color = props->color_map.matrix[row][col];
+	props->pixel.next_color = props->color_map.matrix[row][col + 1];
+	draw_bresenham(&line, props);
+}
+
+static void	connect_row(t_props *props, int row, int col)
+{
+	t_line	line;
+
+	line.x0 = props->points[(row * props->map.cols) + col]->x;
+	line.y0 = props->points[(row * props->map.cols) + col]->y;
+	line.x1 = props->points[(row + 1) * props->map.cols + col]->x;
+	line.y1 = props->points[(row + 1) * props->map.cols + col]->y;
+	props->pixel.color = props->color_map.matrix[row][col];
+	props->pixel.next_color = props->color_map.matrix[row + 1][col];
+	draw_bresenham(&line, props);
+}
+
+void	connect_points(t_props *props)
+{
+	int		row;
+	int		col;
+	t_line	line;
+
+	row = 0;
+	while (row < props->map.rows)
+	{
+		col = 0;
+		while (col < props->map.cols)
+		{
+			if (col + 1 < props->map.cols)
+				connect_col(props, row, col);
+			if (row + 1 < props->map.rows)
+				connect_row(props, row, col);
+			free(props->points[row * props->map.cols + col]);
+			col++;
+		}
+		row++;
+	}
+	mlx_put_image_to_window(props->mlx, props->window, props->image, 0, 0);
+}
---
+++ vectors.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vectors.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 05:32:12 by btan              #+#    #+#             */
+/*   Updated: 2024/01/16 06:48:49 by btan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fdf.h>
+
+static float	**ortho_projection(float ***matrix)
+{
+	float	**transformation;
+	float	**projection;
+
+	transformation = orthographic();
+	projection = matrix_mult(transformation, *matrix);
+	free_matrix(transformation);
+	return (projection);
+}
+
+static void	process_vector(t_props *props, int row, int col)
+{
+	t_vec3	*vec3;
+	float	**matrix;
+	float	**projection;
+
+	vec3 = ft_calloc(1, sizeof(t_vec3));
+	vec3->x = col;
+	vec3->y = row;
+	vec3->z = props->map.matrix[row][col];
+	set_local_origin(props, &vec3);
+	matrix = vec3_to_matrix(vec3);
+	projection = ortho_projection(&matrix);
+	free_matrix(matrix);
+	free(vec3);
+	rotate_xyz(&projection, &props->rotation);
+	set_scale(&projection, pixels_per_unit(props) * props->scale);
+	set_world_origin(props, &projection);
+	props->points[(row * props->map.cols) + col] = matrix_to_vec2(projection);
+	free_matrix(projection);
+}
+
+void	plot_vectors(t_props *props)
+{
+	int		row;
+	int		col;
+	t_vec3	*vec3;
+	float	**matrix;
+	float	**projection;
+
+	row = 0;
+	while (row < props->map.rows)
+	{
+		col = 0;
+		while (col < props->map.cols)
+		{
+			process_vector(props, row, col);
+			col++;
+		}
+		row++;
+	}
+}
